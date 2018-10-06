8e2b881b1eab79e37198a2de7b29722f4df4dda7
Gökhan Gümül <gumulgokhan@gmail.com>
2018-10-06 23:36:09 +0300

    upd

---
+++ Parser/Program.cs
@@ -0,0 +1,75 @@
+﻿using HtmlAgilityPack;
+using Parser.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    class Program
+    {
+        public static void Secim()
+        {
+            Console.WriteLine("######Seçim Ekranı######");
+            Console.WriteLine("Dolar için 1'e  basınız");
+            Console.WriteLine("Euro  için 2'ye basınız");
+            Console.WriteLine("Çıkış için 3'e  basınız");
+            Console.WriteLine();
+            var deger = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine();
+            if (deger == 1)
+            {
+                Dolar();
+            }
+           else if (deger == 2)
+            {
+                Euro();
+            }
+           else if (deger == 3)
+            {
+                Environment.Exit(0); 
+            }
+            else
+            {
+                Console.WriteLine("1'e 2'ye veya 3'e basınız.");
+            }
+           
+            
+          
+        }
+        public static void Dolar()
+        {
+            Parse getir = new Parse();
+            string[] gelenler = new string[3];
+            gelenler = getir.Dolar();
+            Console.WriteLine("Dolar alış:" + gelenler[0]);
+            Console.WriteLine("Dolar satış:" + gelenler[1]);
+            Console.WriteLine("Dolar önceki kapanış:" + gelenler[2]);
+            Console.WriteLine();
+            
+        }
+        public static void Euro()
+        {
+            Parse getir = new Parse();
+            string[] gelenler = new string[3];
+            gelenler = getir.Euro();
+            Console.WriteLine("Euro alış:" + gelenler[0]);
+            Console.WriteLine("Euro satış:" + gelenler[1]);
+            Console.WriteLine("Euro önceki kapanış:" + gelenler[2]);
+            Console.WriteLine();
+
+        }
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Secim();
+            }
+            
+           
+        }
+        
+    }
+}
---
+++ Parser/Tools/Parse.cs
@@ -0,0 +1,47 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Tools
+{
+   public class Parse
+   {
+        private HtmlWeb web;
+        private string siteadi;
+        public Parse()
+        {
+            web = new HtmlWeb();
+        }
+        public string[] Dolar()
+        {
+            siteadi = @"http://bigpara.hurriyet.com.tr/doviz/dolar/";
+            string[] dizi = new string[3];
+            var site = web.Load(siteadi);
+            var documenta = site.DocumentNode.SelectSingleNode("//span[@class='value dw']").InnerText;
+            dizi[0] = documenta;
+            var documents = site.DocumentNode.SelectSingleNode("//span[@class='value up']").InnerText;
+            dizi[1] = documents;
+            var documentk = site.DocumentNode.SelectSingleNode("//span[@class='text2']").InnerText;
+            dizi[2] = documentk;
+            return dizi;
+           
+        }
+        public string[] Euro()
+        {
+            siteadi = @"http://bigpara.hurriyet.com.tr/doviz/euro/";
+            string[] dizi = new string[3];
+            var site = web.Load(siteadi);
+            var documenta = site.DocumentNode.SelectSingleNode("//span[@class='value dw']").InnerText;
+            dizi[0] = documenta;
+            var documents = site.DocumentNode.SelectSingleNode("//span[@class='value up']").InnerText;
+            dizi[1] = documents;
+            var documentk = site.DocumentNode.SelectSingleNode("//span[@class='text2']").InnerText;
+            dizi[2] = documentk;
+            return dizi;
+
+        }
+   }
+}
